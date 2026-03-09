1c8be845b19c91d5c02a295c9793676aabac5ea4
Pushkarsingh9900 <pushkarthakur9760@gmail.com>
2026-03-09 17:14:39 -0400

    setting up the skeleton for backend

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("NeighborNet API running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
